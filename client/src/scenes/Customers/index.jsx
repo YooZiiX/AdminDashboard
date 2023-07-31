3d406fa286b056867df54709c362c69cc4d2dacc
YooZiiX <jeremy.lucas@mines-ales.org>
2023-07-31 22:04:18 +0200

    styled

--- client/src/scenes/Customers/index.jsx
+++ client/src/scenes/Customers/index.jsx
@@ -4,8 +4,9 @@ import Header from "components/Header";
 
 const Customers = () => {
   return (
-    <Box>
-      <Header title="Customers" subtitles="List of Customers" />
+    <Box m="1.5rem 2.5rem">
+      <Header title="Customers" subtitle="List of Customers." />
+      <Box></Box>
     </Box>
   );
 };
