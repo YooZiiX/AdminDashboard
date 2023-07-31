57ec124644f7a10f37ab32dfd358fba6ca75b3bb
YooZiiX <jeremy.lucas@mines-ales.org>
2023-07-31 19:08:11 +0200

    init products routes

--- server/routes/client.js
+++ server/routes/client.js
@@ -1,5 +1,7 @@
 import express from "express";
+import { getProducts } from "../controllers/clientController.js";
 
 const router = express.Router();
+router.get("/products", getProducts);
 
 export default router;
