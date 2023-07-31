d0c2e9b8b45b935f8d6cbe91e58a2b0e987288cf
YooZiiX <jeremy.lucas@mines-ales.org>
2023-07-31 21:57:46 +0200

    init Customers scenes & add Header

---
+++ client/src/scenes/Customers/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Box } from "@mui/material";
+import Header from "components/Header";
+
+const Customers = () => {
+  return (
+    <Box>
+      <Header title="Customers" subtitles="List of Customers" />
+    </Box>
+  );
+};
+
+export default Customers;
