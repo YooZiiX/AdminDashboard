791f573f363c8d809c63f809b1d12303e66dd722
YooZiiX <jeremy.lucas@mines-ales.org>
2023-07-31 19:53:53 +0200

    add Header

--- client/src/scenes/Products/index.jsx
+++ client/src/scenes/Products/index.jsx
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import {
+  Box,
+  Card,
+  CardActions,
+  CardContent,
+  Collapse,
+  Button,
+  Typography,
+  Rating,
+  useTheme,
+  useMediaQuery,
+} from "@mui/material";
+import Header from "../../components/Header";
+import { useGetProductsQuery } from "state/api";
 
 const Products = () => {
-  return <div>Products</div>;
+  return (
+    <Box>
+      <Header title="Products" subtitle=" See your list of products." />
+      <Box></Box>
+    </Box>
+  );
 };
 
 export default Products;
