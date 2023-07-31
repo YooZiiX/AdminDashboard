15645f6d93d40cdd29c8f5f6545f5b1c07b44175
YooZiiX <jeremy.lucas@mines-ales.org>
2023-07-31 19:38:14 +0200

    add /products route

--- client/src/Router.js
+++ client/src/Router.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import Layout from "scenes/Layout";
 import Dashboard from "scenes/Dashboard";
+import Products from "scenes/Products";
 
 function Router({ theme }) {
   return (
@@ -13,6 +14,7 @@ function Router({ theme }) {
           <Route element={<Layout />}>
             <Route path="/" element={<Navigate to="/dashboard" replace />} />
             <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/products" element={<Products />} />
           </Route>
         </Routes>
       </ThemeProvider>
