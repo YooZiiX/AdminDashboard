2ebf090ad620455cfb3c948ff38f3f09dc2fc694
YooZiiX <jeremy.lucas@mines-ales.org>
2023-08-01 22:52:05 +0200

    add Header

--- client/src/scenes/Dashboard/index.jsx
+++ client/src/scenes/Dashboard/index.jsx
@@ -1,8 +1,15 @@
 import React from "react";
+import { Box } from "@mui/material";
+import Header from "components/Header";
 
 const Dashboard = () => {
   document.title = "Dashboard";
-  return <div>Dashboard</div>;
+  return (
+    <Box m="1.5rem 2.5rem">
+      <Header title="Dashboard" subtitle="" />
+      <Box></Box>
+    </Box>
+  );
 };
 
 export default Dashboard;
