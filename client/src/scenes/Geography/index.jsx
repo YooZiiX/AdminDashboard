bb61de3a1fac5bc63b75f825a2a199779700976c
YooZiiX <jeremy.lucas@mines-ales.org>
2023-08-01 22:49:38 +0200

    add Header

--- client/src/scenes/Geography/index.jsx
+++ client/src/scenes/Geography/index.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import { Box } from "@mui/material";
+import Header from "components/Header";
 
 const Geography = () => {
-  return <div>Geography</div>;
+  document.title = "Geography";
+  return (
+    <Box m="1.5rem 2.5rem">
+      <Header title="Geography" subtitle="Find where your users are located." />
+      <Box></Box>
+    </Box>
+  );
 };
 
 export default Geography;
