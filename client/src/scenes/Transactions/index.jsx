81630c39313e31d14c79d38c238590c714b6a85c
YooZiiX <jeremy.lucas@mines-ales.org>
2023-08-01 21:39:57 +0200

    add custom toolbar

--- client/src/scenes/Transactions/index.jsx
+++ client/src/scenes/Transactions/index.jsx
@@ -3,6 +3,7 @@ import { Box, useTheme } from "@mui/material";
 import { DataGrid } from "@mui/x-data-grid";
 import { useGetTransactionsQuery } from "state/api";
 import Header from "../../components/Header";
+import DataGridCustomToolbar from "../../components/DataGridCustomToolbar";
 
 const Transactions = () => {
   const theme = useTheme();
@@ -89,7 +90,8 @@ const Transactions = () => {
           sortingMode="server"
           onPageChange={(newPage) => setPage(newPage)}
           onPageSizeChange={(newPageSize) => setPageSize(newPageSize)}
-          onSortModelChange={(newSortModel) => setSort(newSortModel)}
+          onSortModelChange={(newSortModel) => setSort(...newSortModel)}
+          components={{ Toolbar: DataGridCustomToolbar }}
         />
       </Box>
     </Box>
