3c70c7b05cb6635bb7f0efdf1cee04e0558bc7a7
YooZiiX <jeremy.lucas@mines-ales.org>
2023-08-02 21:06:16 +0200

    add Theme

--- client/src/scenes/Geography/index.jsx
+++ client/src/scenes/Geography/index.jsx
@@ -1,9 +1,10 @@
 import React from "react";
-import { Box } from "@mui/material";
+import { Box, useTheme } from "@mui/material";
 import Header from "components/Header";
 
 const Geography = () => {
   document.title = "Geography";
+  const theme = useTheme();
   return (
     <Box m="1.5rem 2.5rem">
       <Header title="Geography" subtitle="Find where your users are located." />
