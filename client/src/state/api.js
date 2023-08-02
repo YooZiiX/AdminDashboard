480bbd2c68b5b6fe0faabfc823377ffe59dd03b3
YooZiiX <jeremy.lucas@mines-ales.org>
2023-08-02 21:58:08 +0200

    add getSales endpoint

--- client/src/state/api.js
+++ client/src/state/api.js
@@ -3,7 +3,14 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const api = createApi({
   baseQuery: fetchBaseQuery({ baseUrl: process.env.REACT_APP_BASE_URL }),
   reducerPath: "adminApi",
-  tagTypes: ["User", "Products", "Customers", "Transactions", "Geography"],
+  tagTypes: [
+    "User",
+    "Products",
+    "Customers",
+    "Transactions",
+    "Geography",
+    "Sales",
+  ],
   endpoints: (build) => ({
     getUser: build.query({
       query: (id) => `general/user/${id}`,
@@ -30,7 +37,7 @@ export const api = createApi({
       providesTags: ["Geography"],
     }),
     getSales: build.query({
-      query: "sales/overall",
+      query: () => "sales/sales",
       providesTags: ["Sales"],
     }),
   }),
