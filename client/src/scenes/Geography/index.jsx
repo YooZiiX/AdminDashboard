7ea3d6dff021dfe9c0899dc04b671b56f6d14b10
YooZiiX <jeremy.lucas@mines-ales.org>
2023-08-02 21:13:00 +0200

    add data

--- client/src/scenes/Geography/index.jsx
+++ client/src/scenes/Geography/index.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import { Box, useTheme } from "@mui/material";
+import { useGetGeographyQuery } from "state/api";
 import Header from "components/Header";
+import { ResponsiveChoropleth } from "@nivo/geo";
+import { geoData } from "state/geoData";
 
 const Geography = () => {
   document.title = "Geography";
   const theme = useTheme();
+  const { data } = useGetGeographyQuery();
   return (
     <Box m="1.5rem 2.5rem">
       <Header title="Geography" subtitle="Find where your users are located." />
