c79d8c967ab3b1cb331822257d49d641e7e7dc51
YooZiiX <jeremy.lucas@mines-ales.org>
2023-08-02 22:04:13 +0200

    add importation & subtitle

--- client/src/scenes/Overview/index.jsx
+++ client/src/scenes/Overview/index.jsx
@@ -1,11 +1,24 @@
-import React from "react";
-import { Box, useTheme } from "@mui/material";
+import React, { useState } from "react";
+import {
+  Box,
+  useTheme,
+  FormControl,
+  MenuItem,
+  InputLabel,
+  Select,
+} from "@mui/material";
 import Header from "components/Header";
+import OverviewChart from "components/OverviewChart";
 
 const Overview = () => {
+  document.title = "Overview";
+  const [view, setView] = useState("units");
   return (
     <Box m="1.5rem 2.5rem">
-      <Header title="Overview" subtitle="" />
+      <Header
+        title="Overview"
+        subtitle="Overview of general revenue and profit."
+      />
       <Box></Box>
     </Box>
   );
