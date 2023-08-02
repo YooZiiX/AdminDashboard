05e0219567ac0c3886784bc255e723016c2ba176
YooZiiX <jeremy.lucas@mines-ales.org>
2023-08-02 21:59:54 +0200

    init Overview Scene

---
+++ client/src/scenes/Overview/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Box, useTheme } from "@mui/material";
+import Header from "components/Header";
+
+const Overview = () => {
+  return (
+    <Box m="1.5rem 2.5rem">
+      <Header title="Overview" subtitle="" />
+      <Box></Box>
+    </Box>
+  );
+};
+
+export default Overview;
