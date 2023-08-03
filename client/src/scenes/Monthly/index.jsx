fa82e0790f7293f98ff77700f3aa2a0d83b0bbd4
YooZiiX <jeremy.lucas@mines-ales.org>
2023-08-03 22:10:40 +0200

    add subtitle

--- client/src/scenes/Monthly/index.jsx
+++ client/src/scenes/Monthly/index.jsx
@@ -5,7 +5,7 @@ import Header from "components/Header";
 const Monthly = () => {
   return (
     <Box m="1.5rem 2.5rem">
-      <Header title="Monthly" subtitle="" />
+      <Header title="Monthly" subtitle="Chart of monthly sales." />
       <Box height="75vh"></Box>
     </Box>
   );
