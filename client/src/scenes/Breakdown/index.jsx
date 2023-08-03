5911983608bb932af932061809a97463dafc371a
YooZiiX <jeremy.lucas@mines-ales.org>
2023-08-03 22:30:23 +0200

    chane document title

--- client/src/scenes/Breakdown/index.jsx
+++ client/src/scenes/Breakdown/index.jsx
@@ -3,6 +3,7 @@ import { Box } from "@mui/material";
 import Header from "components/Header";
 
 const Breakdown = () => {
+  document.title = "Breakdown";
   return (
     <Box m="1.5rem 2.5rem">
       <Header title="Breakdown" subtitle="Breakdown of Sales by Category." />
