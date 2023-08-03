4706468aead1a0781e56511d76b5e3a7a6ebd51e
YooZiiX <jeremy.lucas@mines-ales.org>
2023-08-03 22:13:46 +0200

    import data

--- client/src/scenes/Monthly/index.jsx
+++ client/src/scenes/Monthly/index.jsx
@@ -1,8 +1,9 @@
 import React from "react";
-import { Box } from "@mui/material";
+import { Box, useTheme } from "@mui/material";
 import Header from "components/Header";
 
 const Monthly = () => {
+  const theme = useTheme();
   return (
     <Box m="1.5rem 2.5rem">
       <Header title="Monthly" subtitle="Chart of monthly sales." />
