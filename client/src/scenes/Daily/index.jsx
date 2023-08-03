bbba586cc101271aff6149d4bf911ae12ad54cdc
YooZiiX <jeremy.lucas@mines-ales.org>
2023-08-03 21:51:38 +0200

    chart system implementation

--- client/src/scenes/Daily/index.jsx
+++ client/src/scenes/Daily/index.jsx
@@ -10,6 +10,43 @@ const Daily = () => {
   const [startDate, setSstartDate] = useState(new Date("2021-02-01"));
   const [endDate, setEndDate] = useState(new Date("2021-03-01"));
   const { data } = useGetSalesQuery();
+
+  const [formattedData] = useMemo(() => {
+    if (!data) return [];
+
+    const { dailyData } = data;
+
+    const totalSalesLine = {
+      id: "totalSales",
+      color: theme.palette.secondary.main,
+      data: [],
+    };
+    const totalUnitsLine = {
+      id: "totalUnits",
+      color: theme.palette.secondary[600],
+      data: [],
+    };
+
+    Object.values(dailyData).forEach(({ date, totalSales, totalUnits }) => {
+      const dateFormatted = new Date(date);
+      if (dateFormatted >= startDate && dateFormatted <= endDate) {
+        const splitDate = date.substring(date.indexOf("-") + 1);
+
+        totalSalesLine.data = [
+          ...totalSalesLine.data,
+          { x: splitDate, y: totalSales },
+        ];
+        totalUnitsLine.data = [
+          ...totalUnitsLine.data,
+          { x: splitDate, y: totalUnits },
+        ];
+      }
+    });
+
+    const formattedData = [totalSalesLine, totalUnitsLine];
+    return [formattedData];
+  }, [data, startDate, endDate]);
+
   return (
     <Box m="1.5rem 2.5rem">
       <Header title="Daily" subtitle="" />
