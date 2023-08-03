bd4d2f1faf5aebda540e2e4f8f92ea465cc4171d
YooZiiX <jeremy.lucas@mines-ales.org>
2023-08-03 21:43:05 +0200

    add dependencies

--- client/src/scenes/Daily/index.jsx
+++ client/src/scenes/Daily/index.jsx
@@ -1,8 +1,14 @@
-import React from "react";
-import { Box } from "@mui/material";
+import React, { useMemo, useState } from "react";
+import ReactDatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.module.css";
+import { Box, useTheme } from "@mui/material";
 import Header from "components/Header";
+import { useGetSalesQuery } from "state/api";
 
 const Daily = () => {
+  const theme = useTheme();
+  const [startDate, setSstartDate] = useState(new Date("2021-02-01"));
+  const [endDate, setEndDate] = useState(new Date("2021-03-01"));
   return (
     <Box m="1.5rem 2.5rem">
       <Header title="Daily" subtitle="" />
