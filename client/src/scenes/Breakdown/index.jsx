5fd73c27369e03f2eac82e0eacd191b120dabb38
YooZiiX <jeremy.lucas@mines-ales.org>
2023-08-03 22:26:53 +0200

    add Header

--- client/src/scenes/Breakdown/index.jsx
+++ client/src/scenes/Breakdown/index.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import { Box } from "@mui/material";
+import Header from "components/Header";
 
 const Breakdown = () => {
-  return <div>Breakdown</div>;
+  return (
+    <Box m="1.5rem 2.5rem">
+      <Header title="Breakdown" subtitle="Breakdown of Sales by Category." />
+      <Box></Box>
+    </Box>
+  );
 };
 
 export default Breakdown;
