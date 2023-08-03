70de531239631c3cbc8d5c4f8504b6994deeac5e
YooZiiX <jeremy.lucas@mines-ales.org>
2023-08-03 22:08:26 +0200

    add Header

--- client/src/scenes/Monthly/index.jsx
+++ client/src/scenes/Monthly/index.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import { Box } from "@mui/material";
+import Header from "components/Header";
 
 const Monthly = () => {
-  return <div>Monthly</div>;
+  return (
+    <Box m="1.5rem 2.5rem">
+      <Header title="Monthly" subtitle="" />
+      <Box height="75vh"></Box>
+    </Box>
+  );
 };
 
 export default Monthly;
