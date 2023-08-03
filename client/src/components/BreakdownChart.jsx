2e1f3e35de1c67d3862ed6365c68fc296749ae02
YooZiiX <jeremy.lucas@mines-ales.org>
2023-08-03 22:46:21 +0200

    add format system

--- client/src/components/BreakdownChart.jsx
+++ client/src/components/BreakdownChart.jsx
@@ -4,7 +4,36 @@ import { ResponsivePie } from "@nivo/pie";
 import { useGetSalesQuery } from "state/api";
 
 const BreakdownChart = ({ isDashboard = false }) => {
-  return <div>BreakdownChart</div>;
+  const theme = useTheme();
+  const { data, isLoading } = useGetSalesQuery();
+
+  if (!data || isLoading) return "Loading...";
+
+  const colors = [
+    theme.palette.secondary[500],
+    theme.palette.secondary[300],
+    theme.palette.secondary[300],
+    theme.palette.secondary[500],
+  ];
+
+  const formattedData = Object.entries(data.salesByCategory).map(
+    ([category, sales], i) => ({
+      id: category,
+      label: category,
+      value: sales,
+      color: colors[i],
+    })
+  );
+
+  return (
+    <Box
+      height={isDashboard ? "400px" : "100%"}
+      width={undefined}
+      minHeight={isDashboard ? "325px" : undefined}
+      minWidth={isDashboard ? "325px" : undefined}
+      position="relative"
+    ></Box>
+  );
 };
 
 export default BreakdownChart;
