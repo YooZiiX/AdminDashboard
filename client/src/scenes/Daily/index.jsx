938b70e60d431f2ace42aab3b537db1b34b42db1
YooZiiX <jeremy.lucas@mines-ales.org>
2023-08-03 21:33:16 +0200

    add Header

--- client/src/scenes/Daily/index.jsx
+++ client/src/scenes/Daily/index.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import { Box } from "@mui/material";
+import Header from "components/Header";
 
 const Daily = () => {
-  return <div>Daily</div>;
+  return (
+    <Box m="1.5rem 2.5rem">
+      <Header title="Daily" subtitle="" />
+      <Box></Box>
+    </Box>
+  );
 };
 
 export default Daily;
