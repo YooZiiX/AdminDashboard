e638f98edd41000d4ab1039256f4f622032304a5
YooZiiX <jeremy.lucas@mines-ales.org>
2023-08-03 22:32:38 +0200

    add Breakdown chart

--- client/src/scenes/Breakdown/index.jsx
+++ client/src/scenes/Breakdown/index.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import { Box } from "@mui/material";
 import Header from "components/Header";
+import BreakdownChart from "components/BreakdownChart";
 
 const Breakdown = () => {
   document.title = "Breakdown";
   return (
     <Box m="1.5rem 2.5rem">
       <Header title="Breakdown" subtitle="Breakdown of Sales by Category." />
-      <Box></Box>
+      <Box mt="40px" height="75vh">
+        <BreakdownChart />
+      </Box>
     </Box>
   );
 };
