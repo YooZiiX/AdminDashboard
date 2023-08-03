51e889e0a9db7f87307e6c263ce624e08c855930
YooZiiX <jeremy.lucas@mines-ales.org>
2023-08-03 22:35:16 +0200

    add impotations

---
+++ client/src/components/BreakdownChart.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Box, Typography, useTheme } from "@mui/material";
+import { ResponsivePie } from "@nivo/pie";
+import { useGetSalesQuery } from "state/api";
+
+const BreakdownChart = ({ isDashboard = false }) => {
+  return <div>BreakdownChart</div>;
+};
+
+export default BreakdownChart;
