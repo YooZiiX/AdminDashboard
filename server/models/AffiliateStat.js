4bd7d7c4479a91db633179e9f1235f75273483b9
YooZiiX <jeremy.lucas@mines-ales.org>
2023-08-04 21:27:33 +0200

    fix model error

--- server/models/AffiliateStat.js
+++ server/models/AffiliateStat.js
@@ -2,11 +2,8 @@ import mongoose from "mongoose";
 
 const AffiliateStatSchema = new mongoose.Schema(
   {
-    userId: {
-      type: mongoose.Types.ObjectId,
-      ref: "User",
-    },
-    affiliateStat: {
+    userId: { type: mongoose.Types.ObjectId, ref: "User" },
+    affiliateSales: {
       type: [mongoose.Types.ObjectId],
       ref: "Transaction",
     },
@@ -14,5 +11,5 @@ const AffiliateStatSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
-const AffiliateStat = new mongoose.model("AffiliateStat", AffiliateStatSchema);
+const AffiliateStat = mongoose.model("AffiliateStat", AffiliateStatSchema);
 export default AffiliateStat;
