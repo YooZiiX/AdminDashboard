a76bd210de7d63391eb72a72d076a7ec7bce58b0
YooZiiX <jeremy.lucas@mines-ales.org>
2023-08-04 21:55:32 +0200

    import data

--- client/src/scenes/Performance/index.jsx
+++ client/src/scenes/Performance/index.jsx
@@ -1,10 +1,15 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import { Box, useTheme } from "@mui/material";
+import { DataGrid } from "@mui/x-data-grid";
 import Header from "components/Header";
+import { useGetUserPerformanceQuery } from "state/api";
 
 const Performance = () => {
   document.title = "Performance";
   const theme = useTheme();
+  const userId = useSelector((state) => state.global.userId);
+  const { data, isLoading } = useGetUserPerformanceQuery(userId);
   return (
     <Box m="1.5rem 2.5rem">
       <Header
