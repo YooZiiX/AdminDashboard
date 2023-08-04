052244673b623f3ce74203cb1246e2cd646997b4
YooZiiX <jeremy.lucas@mines-ales.org>
2023-08-04 19:32:13 +0200

    add Columns

--- client/src/scenes/Admins/index.jsx
+++ client/src/scenes/Admins/index.jsx
@@ -8,6 +8,48 @@ const Admins = () => {
   document.title = "Admins";
   const theme = useTheme();
   const { data, isLoading } = useGetAdminsQuery();
+
+  const columns = [
+    {
+      field: "_id",
+      headerName: "ID",
+      flex: 1,
+    },
+    {
+      field: "name",
+      headerName: "Name",
+      flex: 0.5,
+    },
+    {
+      field: "email",
+      headerName: "Email",
+      flex: 1,
+    },
+    {
+      field: "phoneNumber",
+      headerName: "Phone Number",
+      flex: 0.5,
+      renderCell: (params) => {
+        return params.value.replace(/^(\d{3})(\d{3})(\d{4})/, "($1)$2-$3");
+      },
+    },
+    {
+      field: "country",
+      headerName: "Country",
+      flex: 0.4,
+    },
+    {
+      field: "occupation",
+      headerName: "Occupation",
+      flex: 1,
+    },
+    {
+      field: "role",
+      headerName: "Role",
+      flex: 0.5,
+    },
+  ];
+
   return (
     <Box m="1.5rem 2.5rem">
       <Header title="Admins" subtitle="Managing admins and list of admins." />
