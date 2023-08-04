94eaf93f8d6c7f6b9be63d935df85d7383bff5ed
YooZiiX <jeremy.lucas@mines-ales.org>
2023-08-04 21:11:59 +0200

    add Header

--- client/src/scenes/Performance/index.jsx
+++ client/src/scenes/Performance/index.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import { Box, useTheme } from "@mui/material";
+import Header from "components/Header";
 
 const Performance = () => {
-  return <div>Performance</div>;
+  return (
+    <Box>
+      <Header
+        title="Performance"
+        subtitle="Track your affiliate Sales performance."
+      />
+      <Box></Box>
+    </Box>
+  );
 };
 
 export default Performance;
