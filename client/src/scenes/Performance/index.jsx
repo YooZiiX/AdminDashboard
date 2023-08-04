0714ca96b0d34cc0dec43382cf5814341b38fe2a
YooZiiX <jeremy.lucas@mines-ales.org>
2023-08-04 21:14:00 +0200

    change document title

--- client/src/scenes/Performance/index.jsx
+++ client/src/scenes/Performance/index.jsx
@@ -3,8 +3,10 @@ import { Box, useTheme } from "@mui/material";
 import Header from "components/Header";
 
 const Performance = () => {
+  document.title = "Performance";
+  const theme = useTheme();
   return (
-    <Box>
+    <Box m="1.5rem 2.5rem">
       <Header
         title="Performance"
         subtitle="Track your affiliate Sales performance."
