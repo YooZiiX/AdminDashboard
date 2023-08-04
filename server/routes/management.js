0694cbf7a358790bd060930522639c9b65129777
YooZiiX <jeremy.lucas@mines-ales.org>
2023-08-04 19:20:37 +0200

    add Admins route

--- server/routes/management.js
+++ server/routes/management.js
@@ -1,5 +1,7 @@
 import express from "express";
+import { getAdmins } from "../controllers/managementController.js";
 
 const router = express.Router();
+router.get("/admins", getAdmins);
 
 export default router;
