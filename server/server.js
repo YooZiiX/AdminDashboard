26fe1b8a7a882069ad180e1a1f7503e5f0ebfa0f
YooZiiX <jeremy.lucas@mines-ales.org>
2023-08-04 21:22:40 +0200

    add affiliate data

--- server/server.js
+++ server/server.js
@@ -15,12 +15,14 @@ import Product from "./models/Product.js";
 import ProductStat from "./models/ProductStat.js";
 import Transaction from "./models/Transaction.js";
 import OverallStat from "./models/OverallStat.js";
+import AffiliateStat from "./models/AffiliateStat.js";
 import {
   dataUser,
   dataProduct,
   dataProductStat,
   dataTransaction,
   dataOverallStat,
+  dataAffiliateStat,
 } from "./data/data.js";
 
 /* CONFIGURATION */
@@ -56,5 +58,6 @@ mongoose
     // ProductStat.insertMany(dataProductStat);
     // Transaction.insertMany(dataTransaction);
     // OverallStat.insertMany(dataOverallStat);
+    AffiliateStat.insertMany(dataAffiliateStat);
   })
   .catch((error) => console.log(`Error -> ${error}`));
