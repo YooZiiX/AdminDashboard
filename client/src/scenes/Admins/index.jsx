a83b5e35f6c30e612619cf6c23cbba25ce7099d1
YooZiiX <jeremy.lucas@mines-ales.org>
2023-08-04 19:09:15 +0200

    add Header

--- client/src/scenes/Admins/index.jsx
+++ client/src/scenes/Admins/index.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import { Box } from "@mui/material";
+import Header from "components/Header";
 
 const Admins = () => {
-  return <div>Admins</div>;
+  return (
+    <Box m="1.5rem 2.5rem">
+      <Header title="Admins" subtitle="Managing admins and list of admins." />
+      <Box></Box>
+    </Box>
+  );
 };
 
 export default Admins;
