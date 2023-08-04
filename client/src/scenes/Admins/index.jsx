4bc756e15acce51a59aaadc8e392fe9b4209cc98
YooZiiX <jeremy.lucas@mines-ales.org>
2023-08-04 19:34:40 +0200

    add DataGrid

--- client/src/scenes/Admins/index.jsx
+++ client/src/scenes/Admins/index.jsx
@@ -53,7 +53,40 @@ const Admins = () => {
   return (
     <Box m="1.5rem 2.5rem">
       <Header title="Admins" subtitle="Managing admins and list of admins." />
-      <Box></Box>
+      <Box
+        mt="40px"
+        height="75vh"
+        sx={{
+          "& .MuiDataGrid-root": { border: "none" },
+          "& .MuiDataGrid-cell": { borderBottom: "none" },
+          "& .MuiDataGrid-columnHeaders": {
+            backgroundColor: theme.palette.background.alt,
+            color: theme.palette.secondary[100],
+            borderBottom: "none",
+          },
+          "& .MuiDataGrid-virtualScroller": {
+            backgroundColor: theme.palette.primary.light,
+          },
+          "& .MuiDataGrid-footerContainer": {
+            backgroundColor: theme.palette.background.alt,
+            color: theme.palette.secondary[100],
+            borderBottom: "none",
+          },
+          "& .MuiDataGrid-toolbarContainer .MuiButton-text": {
+            color: `theme.palette.secondary[200] !important`,
+          },
+        }}
+      >
+        <DataGrid
+          loading={isLoading || !data}
+          getRowId={(row) => row._id}
+          rows={data || []}
+          columns={columns}
+          slots={{
+            columnMenu: CustomColumnMenu,
+          }}
+        />
+      </Box>
     </Box>
   );
 };
