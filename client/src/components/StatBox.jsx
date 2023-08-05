491c8abf470fe9d4627f215b5812679b1a847b1c
YooZiiX <jeremy.lucas@mines-ales.org>
2023-08-05 22:09:56 +0200

    add importations

--- client/src/components/StatBox.jsx
+++ client/src/components/StatBox.jsx
@@ -1,7 +1,9 @@
 import React from "react";
+import { Box, Typography, useTheme } from "@mui/material";
+import FlexBetween from "./FlexBetween";
 
-const StatBox = () => {
-  return <div>StatBox</div>;
+const StatBox = ({ title, value, increase, icon, description }) => {
+  return <Box>StatBox</Box>;
 };
 
 export default StatBox;
