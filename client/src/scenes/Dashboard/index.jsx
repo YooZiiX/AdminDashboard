9c75c911dba7eda848f81f33079fac5bbc6846ef
YooZiiX <jeremy.lucas@mines-ales.org>
2023-08-05 20:26:08 +0200

    add importations

--- client/src/scenes/Dashboard/index.jsx
+++ client/src/scenes/Dashboard/index.jsx
@@ -1,6 +1,24 @@
 import React from "react";
-import { Box } from "@mui/material";
+import {
+  Box,
+  Button,
+  Typography,
+  useTheme,
+  useMediaQuery,
+} from "@mui/material";
+import { DataGrid } from "@mui/x-data-grid";
+import {
+  DownloadOutlined,
+  Email,
+  PointOfSale,
+  PersonAdd,
+  Traffic,
+} from "@mui/icons-material";
+import BreakdownChart from "components/BreakdownChart";
+import FlexBetween from "components/FlexBetween";
 import Header from "components/Header";
+import OverviewChart from "components/OverviewChart";
+import { useGetDashboardQuery } from "state/api";
 
 const Dashboard = () => {
   document.title = "Dashboard";
