8653718073f794c09ed7aa7d2fff5773e6dc9cd2
YooZiiX <jeremy.lucas@mines-ales.org>
2023-08-05 20:41:33 +0200

    add DL button

--- client/src/scenes/Dashboard/index.jsx
+++ client/src/scenes/Dashboard/index.jsx
@@ -60,7 +60,20 @@ const Dashboard = () => {
   return (
     <Box m="1.5rem 2.5rem">
       <Header title="Dashboard" subtitle="Welcome to your Dashboard." />
-      <Box></Box>
+      <Box>
+        <Button
+          sx={{
+            backgroundColor: theme.palette.secondary.light,
+            color: theme.palette.background.alt,
+            fontSize: "14px",
+            fontWeight: "bold",
+            padding: "10px 20px",
+          }}
+        >
+          <DownloadOutlined sx={{ mr: "10px" }} />
+          Download Reports
+        </Button>
+      </Box>
     </Box>
   );
 };
