e61286bd01716e82b9f70257e206d12674549737
YooZiiX <jeremy.lucas@mines-ales.org>
2023-08-05 20:37:13 +0200

    add columns

--- client/src/scenes/Dashboard/index.jsx
+++ client/src/scenes/Dashboard/index.jsx
@@ -25,6 +25,38 @@ const Dashboard = () => {
   const theme = useTheme();
   const isNonMediumScreens = useMediaQuery("(min-width: 1200px)");
   const { data, isLoading } = useGetDashboardQuery();
+
+  const columns = [
+    {
+      field: "_id",
+      headerName: "ID",
+      flex: 1,
+    },
+    {
+      field: "userId",
+      headerName: "User ID",
+      flex: 1,
+    },
+    {
+      field: "createdAt",
+      headerName: "CreatedAt",
+      flex: 1,
+    },
+    {
+      field: "products",
+      headerName: "# of Products",
+      flex: 0.5,
+      sortable: false,
+      renderCell: (params) => params.value.length,
+    },
+    {
+      field: "cost",
+      headerName: "Cost",
+      flex: 1,
+      renderCell: (params) => `$${Number(params.value).toFixed(2)}`,
+    },
+  ];
+
   return (
     <Box m="1.5rem 2.5rem">
       <Header title="Dashboard" subtitle="Welcome to your Dashboard." />
