b6622479726089fe39ac95ea473f24ae787ae275
YooZiiX <jeremy.lucas@mines-ales.org>
2023-08-05 20:16:41 +0200

    import every data needed

--- server/controllers/generalController.js
+++ server/controllers/generalController.js
@@ -17,7 +17,29 @@ export const getDashboardStats = async (req, res) => {
     const currentMonth = "November";
     const currentYear = 2021;
     const currentDay = "2021-11-15";
+
     /* Recent transaction */
+    const transactions = await Transaction.find()
+      .limit(50)
+      .sort({ createdOn: -1 });
+
+    /* Overall Stats */
+    const overallStat = await OverallStat.find({ year: currentYear });
+    const {
+      totalCustomers,
+      yearlyTotalSoldUnits,
+      yearlySalesTotal,
+      monthlyData,
+      salesByCategory,
+    } = overallStat[0];
+
+    const thisMonthStats = overallStat[0].monthlyData.find(({ month }) => {
+      return month === currentMonth;
+    });
+
+    const todayStats = overallStat[0].dailyData.find(({ date }) => {
+      return date === currentDate;
+    });
   } catch (error) {
     res.status(404).json({ message: error.message });
   }
