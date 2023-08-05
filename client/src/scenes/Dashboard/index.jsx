5e89f73f34bbb7817bc039e141b3456777db7100
YooZiiX <jeremy.lucas@mines-ales.org>
2023-08-05 21:43:46 +0200

    add FlexBetween

--- client/src/scenes/Dashboard/index.jsx
+++ client/src/scenes/Dashboard/index.jsx
@@ -59,21 +59,23 @@ const Dashboard = () => {
 
   return (
     <Box m="1.5rem 2.5rem">
-      <Header title="Dashboard" subtitle="Welcome to your Dashboard." />
-      <Box>
-        <Button
-          sx={{
-            backgroundColor: theme.palette.secondary.light,
-            color: theme.palette.background.alt,
-            fontSize: "14px",
-            fontWeight: "bold",
-            padding: "10px 20px",
-          }}
-        >
-          <DownloadOutlined sx={{ mr: "10px" }} />
-          Download Reports
-        </Button>
-      </Box>
+      <FlexBetween>
+        <Header title="Dashboard" subtitle="Welcome to your Dashboard." />
+        <Box>
+          <Button
+            sx={{
+              backgroundColor: theme.palette.secondary.light,
+              color: theme.palette.background.alt,
+              fontSize: "14px",
+              fontWeight: "bold",
+              padding: "10px 20px",
+            }}
+          >
+            <DownloadOutlined sx={{ mr: "10px" }} />
+            Download Reports
+          </Button>
+        </Box>
+      </FlexBetween>
     </Box>
   );
 };
